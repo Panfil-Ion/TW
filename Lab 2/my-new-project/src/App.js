2b6f3077716d43c270b2ee8a6ce7b35cc9d55fef
Panfil-Ion <ion.panfil@iis.utm.md>
2024-02-13 21:40:31 +0200

    Realizarea laboratorului 2

---
+++ Lab 2/my-new-project/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Bun venit la Grupa Noastră!</h1>
+          <p>Numele meu este [Panfil] [Ion].</p>
+          <p>Sunt în grupa [CR-221].</p>
+        </header>
+      </div>
+  );
+}
+
+export default App;
