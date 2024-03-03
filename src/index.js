1c00c6c9e2964112851cd08b51755b63d7d34b85
Panfil-Ion <ion.panfil@iis.utm.md>
2024-03-03 14:27:58 +0200

    Adaugarea Laboratorului 3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyLayout from './MyLayout';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <MyLayout />
     </React.StrictMode>,
     document.getElementById('root')
 );
